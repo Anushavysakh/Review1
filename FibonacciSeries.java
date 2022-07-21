3f63ba5121e871da5a45f735fe4dace61b2435f2
Anusha <anushavysakh232@gmail.com>
2022-07-21 13:10:49 +0530

    Review1 programs

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -6,13 +6,15 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
 		int n1 = 0, n2 = 1, n3;
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number: ");
-		int num = sc.nextInt();
-		for (int i =0; i < num; i++) {
+		int num = sc.nextInt();		
+		System.out.print(n1+""+n2);
+
+		for (int i =2; i < num; i++) {
 			n3 = n1 + n2;
-			System.out.println("Fibonacci series " + n3);
+			System.out.print(" "+n3);
 			n1 = n2;
 			n2 = n3;
 
